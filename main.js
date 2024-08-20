617f2bc5cd07f96e32a20170b285910fed2578af
joaopedro098 <163419020+joaopedro098@users.noreply.github.com>
2024-08-20 12:20:41 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+let numero =prompt("informe um numero: ")
+document.write(`os numeros digitados foram:<br>`)
+while(numero>0){
+    numero = prompt("informe mais um numero:")
+    document.write( `${numero}<br>`)
+}
